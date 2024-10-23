90d7fd5a36c6b17805b5bc9905341565a61cc6c3
naianemara <maranaiane112@gmail.com>
2024-10-22 22:21:04 -0300

    comparacao entre os input

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function sortear() {
     const min = Math.ceil( document.querySelector(".input-left").value);
     const max = Math.floor( document.querySelector(".input-right").value);
+
+    if(max <= min){
+        alert("O valor minino tem que ser MENOR que o valor maximo")
+    } else {
+       const result =  Math.floor (Math.random() * ( max - min + 1)) + min
+
+       alert (result)
+    }
     
-    alert ( Math.floor (Math.random() * ( max - min + 1) + min))
+   
          
 }
